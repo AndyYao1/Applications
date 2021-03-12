04411481c792f9b83c96f592225f658631cfab28
AndyYao1 <42555116+AndyYao1@users.noreply.github.com>
2021-03-11 23:16:12 -0800

    Modal button working

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 var btn = document.getElementById("convertButton");
+
+var convertModal = document.getElementById("converterModal");
+
+var close = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(){
+    convertModal.style.display = "block";
+}
+
+close.onclick = function(){
+    convertModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == convertModal) {
+      convertModal.style.display = "none";
+    }
+  }
