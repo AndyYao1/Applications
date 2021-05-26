f2364fd30db8650ee748789782ba1b3db460abdc
AndyYao1 <42555116+AndyYao1@users.noreply.github.com>
2021-05-25 21:01:38 -0700

    added decimal conversions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var btn = document.getElementById("convertButton");
+var btn = document.getElementById("convertModalButton");
 
 var convertModal = document.getElementById("converterModal");
 
 var close = document.getElementsByClassName("close")[0];
+
+var convertBtn = document.getElementById("convertButton");
 
 btn.onclick = function(){
     convertModal.style.display = "block";
@@ -17,3 +19,23 @@ window.onclick = function(event) {
       convertModal.style.display = "none";
     }
   }
+
+function toTen(input,base) {
+  var i,output;
+  for (i = 0; i < input.length; i++) {
+    output += input.charAt(i) * Math.pow(base,i);
+  }
+  return output;
+}
+
+function fromTen(input,base){
+  var output;
+  var remainder = input % base;
+  var quotient = input / base;
+  while(quotient > 0){
+    output = remainder + output;
+    quotient = quotient / base;
+    remainder = quotient % base;
+  }
+  return output;
+}
