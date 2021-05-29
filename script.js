392310cf4a372e4a0400006725414c1c8f7ab4f9
AndyYao1 <42555116+AndyYao1@users.noreply.github.com>
2021-05-28 23:15:20 -0700

    converter working properly

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.onclick = function(event) {
     }
   }
 
-convertBtn.onclick() = function(event) {
+function convert() {
   var fromBase = document.getElementById("FromBase").value;
   var toBase = document.getElementById("ToBase").value;
   var input = document.getElementById("Number").value;
@@ -32,32 +32,50 @@ convertBtn.onclick() = function(event) {
       }
     }
   } else {
-    if(input.charCodeAt(i) < 48 || (input.charCodeAt(i) > 57 && input.charCodeAt(i) < 65) || input.charCodeAt(i) > 54 + fromBase ){
-      //error
-      return;
+    for(let i = 0; i < input.length; i++){
+      if(input.charCodeAt(i) < 48 || (input.charCodeAt(i) > 57 && input.charCodeAt(i) < 65) || input.charCodeAt(i) > 54 + fromBase ){
+        //error
+        return;
+      }
     }
   }
   var result = toTen(input,fromBase);
   result = fromTen(result,toBase);
-  return result;
+  document.getElementById("Result").value = result;
 }
 
 function toTen(input,base) {
   var output = 0;
-  for (let i = 0; i < input.length; i++) {
-    output += input.charAt(i) * Math.pow(base,i);
+  for (let i = input.length - 1; i >= 0; i--) {
+    var c;
+    if(base < 11)
+      c = input.charCodeAt(i) - 48;
+    else 
+      c = input.charCodeAt(i) - 55;
+    output = output + (c * Math.pow(base, input.length - i - 1));
   }
   return output;
 }
 
 function fromTen(input,base){
-  var output;
-  var remainder = input % base;
-  var quotient = input / base;
-  while(quotient > 0){
-    output = remainder + output;
-    quotient = quotient / base;
-    remainder = quotient % base;
+  var output = "";
+  var remainder = input;
+  var quotient = input;
+  if (base < 11){ 
+    while(quotient > 0){
+      remainder = quotient % base;
+      quotient = Math.floor(quotient / base);
+      output = remainder.toString() + output;
+    }
+  } else {
+    while(quotient > 0){
+      remainder = quotient % base;
+      quotient = Math.floor(quotient / base);
+      if(remainder < 9)
+        output = remainder.toString() + output;
+      else
+        output = String.fromCharCode(remainder + 55) + output;
+    }
   }
-  return output;
+  return input == 0? 0:output;
 }
