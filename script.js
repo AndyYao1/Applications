cf74a0a3067cd5d11e673f87212369e958e4da5c
AndyYao1 <42555116+AndyYao1@users.noreply.github.com>
2022-01-30 22:03:13 -0800

    fixed error handling on forms, fixed toTen method, works with enter key know

--- script.js
+++ script.js
@@ -6,39 +6,63 @@ var close = document.getElementsByClassName("close")[0];
 
 var convertBtn = document.getElementById("convertButton");
 
+var number = document.getElementById("Number");
+
+window.addEventListener("keydown", function(e){
+  if(e.code === "Enter"){
+    convert();
+  }
+});
+
+number.addEventListener("keydown", function(e){
+  if(e.code === "Enter"){
+    convert();
+  }
+});
+
 btn.onclick = function(){
     convertModal.style.display = "block";
 }
 
 close.onclick = function(){
+    error.textContent = "";
+    document.getElementById("ConvertForm").reset();
+    document.getElementById("Result").value = '';
     convertModal.style.display = "none";
 }
 
 window.onclick = function(event) {
     if (event.target == convertModal) {
+      error.textContent = "";
+      document.getElementById("ConvertForm").reset();
+      document.getElementById("Result").value = '';
       convertModal.style.display = "none";
     }
-  }
+}
 
 function convert() {
-  var fromBase = document.getElementById("FromBase").value;
+  var fromBase = +document.getElementById("FromBase").value;
   var toBase = document.getElementById("ToBase").value;
   var input = document.getElementById("Number").value;
+  var error = document.getElementById("error");
   if(fromBase < 11){
     for(let i = 0; i < input.length; i++){
       if(input.charCodeAt(i) < 48 || input.charCodeAt(i) > 48 + fromBase - 1){
-        //error
+        error.textContent = "Please enter a valid number";
+        error.style.color = "red";
         return;
       }
     }
   } else {
     for(let i = 0; i < input.length; i++){
       if(input.charCodeAt(i) < 48 || (input.charCodeAt(i) > 57 && input.charCodeAt(i) < 65) || input.charCodeAt(i) > 54 + fromBase ){
-        //error
+        error.textContent = "Please enter a valid number";
+        error.style.color = "red";
         return;
       }
     }
   }
+  error.textContent = "";
   var result = toTen(input,fromBase);
   result = fromTen(result,toBase);
   document.getElementById("Result").value = result;
@@ -48,10 +72,9 @@ function toTen(input,base) {
   var output = 0;
   for (let i = input.length - 1; i >= 0; i--) {
     var c;
-    if(base < 11)
-      c = input.charCodeAt(i) - 48;
-    else 
-      c = input.charCodeAt(i) - 55;
+    c = input.charCodeAt(i) - 48;
+    if(c > 9)
+      c-=7;
     output = output + (c * Math.pow(base, input.length - i - 1));
   }
   return output;
